501536fce19c309a25a4486113e8d01538926f41
Akihiro Maeda <sola.1980.a@gmail.com>
2015-10-09 09:40:41 +0900

    Update Android 6.0.0 (MRA58K)

--- aosp.mk
+++ aosp.mk
@@ -1,6 +1,3 @@
 LOCAL_PATH := vendor/aosp/lge/hammerhead
 
-PRODUCT_COPY_FILES += \
-    device/lge/hammerhead/audio_effects.conf:system/vendor/etc/audio_effects.conf
-
 $(call inherit-product, $(LOCAL_PATH)/device-hammerhead.mk)
--- full.mk
+++ full.mk
@@ -4,11 +4,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Titania.ogg \
     ro.config.notification_sound=Tethys.ogg \
     ro.config.alarm_alert=Oxygen.ogg \
-    ro.com.android.dateformat=MM-dd-yyyy \
     ro.com.android.dataroaming=false \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.com.google.clientidbase=android-google \
+    ro.carrier=unknown \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.error.receiver.system.apps=com.google.android.gms \
     ro.setupwizard.enterprise_mode=1 \
@@ -18,16 +18,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.facelock.rec_timeout=2500 \
     ro.facelock.lively_timeout=2500 \
     ro.facelock.est_max_time=600 \
-    ro.facelock.use_intro_anim=false
+    ro.facelock.use_intro_anim=false \
+    ro.build.expect.bootloader=HHZ12k \
+    ro.build.expect.baseband=M8974A-2.0.50.2.27
 
 # Stock Apps
 PRODUCT_PACKAGES += \
     qcrilmsgtunnel \
     shutdownlistener \
     TimeService
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/proprietary/vendor/etc/audio_effects.conf:system/vendor/etc/audio_effects.conf:lge
 
 $(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage13.mk)
 
